78c30457f631dd40e90f40737240be9ca70d072b
tenishevR <tenishev.brrrrrain@gmail.com>
2025-02-07 15:39:00 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,7 @@
+<?php
+    date_default_timezone_set('Europe/Moscow');
+    $Date = date("d.m.Y");
+    $Time = date("H:i:s");
+    echo "Текущая дата: $Date\n";
+    echo "Текушее время: $Time\n";
+    echo "ФИО: Тенишев Роман Ринатович"; ?>
