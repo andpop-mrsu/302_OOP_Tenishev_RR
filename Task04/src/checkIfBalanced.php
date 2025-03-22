b1566f669888ef5ba3cd9df0b58db561a59a7f7e
tenishevR <tenishev.brrrrrain@gmail.com>
2025-03-22 10:51:37 +0300

    Task04

---
+++ Task04/src/checkIfBalanced.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Stack;
+
+function checkIfBalanced(string $expression): bool
+{
+    $stack = new Stack();
+    $pairs = [
+        '>' => '<',
+        '}' => '{',
+        ')' => '(',
+        ']' => '['
+    ];
+
+    for ($i = 0; $i < strlen($expression); $i++) {
+        $char = $expression[$i];
+        if (in_array($char, $pairs)) {
+            $stack->push($char);
+        } elseif (array_key_exists($char, $pairs)) {
+            if ($stack->isEmpty() || $stack->pop() != $pairs[$char]) {
+                return false;
+            }
+        }
+    }
+    return $stack->isEmpty();
+}
---
+++ Task04/src/run_test.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+require_once 'checkIfBalanced.php';
+require_once 'Test.php';
+
+runTest();
